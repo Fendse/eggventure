a25c4db2a9902ea06b8b2b5fbca30cea87fb8a89
Joel Johnsson <46252070+Fendse@users.noreply.github.com>
2021-11-27 17:03:24 +0100

    Make egg stick closer to the ground, I think

--- Assets/DragonMovement.cs
+++ Assets/DragonMovement.cs
@@ -7,20 +7,32 @@ public class DragonMovement : MonoBehaviour
 {
     public State state = State.EGG;
 
-    private Rigidbody2D rb;
+    public float stickiness;
+
+    private Rigidbody2D rigidBody;
     // Start is called before the first frame update
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         if (Math.Abs(Input.GetAxis("Horizontal")) > 0.0f) {
-            Vector2 vOld = rb.velocity;
+            Vector2 vOld = rigidBody.velocity;
             Vector2 vNew = Vector2.MoveTowards(vOld, vOld + new Vector2(2.0f, 0.0f), 0.5f * Math.Sign(Input.GetAxis("Horizontal")));
-            rb.velocity = vNew;
+            rigidBody.velocity = vNew;
         }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Collider2D other = collision.otherCollider;
+        Vector2 stickTo = other.ClosestPoint(gameObject.transform.position);
+
+        Vector2 stickyPosition = Vector2.MoveTowards(transform.position, stickTo, stickiness);
+
+        transform.position = stickyPosition;
     }
 }
