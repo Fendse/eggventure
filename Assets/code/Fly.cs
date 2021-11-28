ff2b4700f4a92717f2114569cadfe5d990d14807
SifenSoul <95126937+SifenSoul@users.noreply.github.com>
2021-11-28 13:40:21 +0100

    HAHAHAHA

--- Assets/code/Fly.cs
+++ Assets/code/Fly.cs
@@ -12,6 +12,7 @@ public class Fly : MonoBehaviour
     // Start is called before the first frame update
     void Start() {
         rb = GetComponent<Rigidbody2D>();
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
